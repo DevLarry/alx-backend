ae337772930005905f5fa645bd371c7f38b9a972
DevLarry <olanrewajusamad070@gmail.com>
2023-10-23 23:48:46 +0100

    Simple helper

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Pagination"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Paginates"""
+    start = (page - 1) * page_size
+    return start, start + page_size
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)

3e41c7d06d9037f907e9076ad753bd53be6f29ce
DevLarry <olanrewajusamad070@gmail.com>
2023-12-20 22:09:23 +0100

    Express setup

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,6 +6,7 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """FIFOcache class"""
     def __init__(self):
         """Cache constructor"""
         super().__init__()
